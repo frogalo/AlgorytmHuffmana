94100719b9aa00c5ba846936963562723c8ff112
Jakub Urbanski <s23525@pjwstk.edu.pl>
2022-06-23 08:53:54 +0200

    finished

--- src/Main.java
+++ src/Main.java
@@ -12,58 +12,47 @@ public class Main {
         scanner.close();
 
         ArrayList<Node> allNodes = countLetters(line);
-
+        int sumBefore = 0;
+        int sumAfter = 0;
 
         for (int i = 0; i < allNodes.size(); i++) {
             allNodes.get(i).setCode(Integer.toBinaryString(i));
-
+            sumBefore += Integer.parseInt(allNodes.get(i).getCode(), 2) * allNodes.get(i).getValue();
             if (allNodes.get(i).getCode().length() == 1)
                 allNodes.get(i).setCode("00" + allNodes.get(i).getCode());
             if (allNodes.get(i).getCode().length() == 2)
                 allNodes.get(i).setCode("0" + allNodes.get(i).getCode());
         }
 
+
         for (Node n : allNodes) {
             if (!String.valueOf(n.getC()).equals("none"))
                 System.out.println(n.getC() + " code: " + n.getCode() + " quantity: " + n.getValue());
             n.setCode("");
         }
-
+        System.out.println("SUM: " + sumBefore);
 
         allNodes.sort(comparator);
 
-       /* for (Node n : allNodes) {
-            System.out.print(n.getC() + "=" + n.getValue() + "\t");
-        }*/
         System.out.println();
 
-//        ArrayList<Node> pairedNodes = new ArrayList<>();
-//        for (int i = 0; i < nodes.size() - 1; i++) {
-//            if (nodes.get(i).getValue() == nodes.get(i + 1).getValue()) {
-//                pairedNodes.add(nodes.get(i));
-//                pairedNodes.add(nodes.get(i + 1));
-//                nodes.remove(i + 1);
-//                nodes.remove(i);
-//            }
-//        }
-
-//        while (pairedNodes.size() != 0) {
-//            nodes.add(new Node('@', pairedNodes.get(0).getValue() + pairedNodes.get(1).getValue()));
-//            pairedNodes.remove(1);
-//            pairedNodes.remove(0);
-//        }
-//        nodes.sort(comparator);
         ArrayList<Node> passedNodes = new ArrayList<>();
         while (allNodes.size() != 1) {
             //System.out.println("BEFORE TURN : " + allNodes);
-           if (allNodes.get(0).getC().length() == 1)
+            if (allNodes.get(0).getC().length() == 1) {
                 allNodes.add(new Node(" Parent of " + allNodes.get(0).getValue() + allNodes.get(0).getC() + " and " + allNodes.get(1).getValue() + allNodes.get(1).getC(),
                         allNodes.get(0).getValue() + allNodes.get(1).getValue(),
                         999, allNodes.get(0), allNodes.get(1)));
-            else
+
+            } else {
                 allNodes.add(new Node(" Parent of " + allNodes.get(0).getValue() + " and " + allNodes.get(1).getValue(),
                         allNodes.get(0).getValue() + allNodes.get(1).getValue(),
                         999, allNodes.get(0), allNodes.get(1)));
+            }//CZY W KOLEJCE POWINNO BYC 0 CZY 999?
+
+
+            allNodes.get(allNodes.size() - 1).setChildLeft(allNodes.get(0));
+            allNodes.get(allNodes.size() - 1).setChildRight(allNodes.get(1));
             allNodes.get(0).setSide("Left");
             allNodes.get(1).setSide("Right");
             passedNodes.add(allNodes.get(0));
@@ -78,60 +67,54 @@ public class Main {
         passedNodes.add(allNodes.get(0));
 
         for (int i = passedNodes.size() - 1; i > 0; i--) {
-           /* System.out.println(passedNodes.get(i).getC() + "\t= " + passedNodes.get(i).getValue() +
+            System.out.println(passedNodes.get(i).getC() + "\t= " + passedNodes.get(i).getValue() +
                     "\t=>\tleft child: " + passedNodes.get(i).getChildLeft() + " " +
-                    ", right child: " + passedNodes.get(i).getChildRight());*/
+                    ", right child: " + passedNodes.get(i).getChildRight());
 
 
             if (passedNodes.get(i).getSide() != null) {
                 if (passedNodes.get(i).getSide().equals("Left")) {
                     passedNodes.get(i).setCode(passedNodes.get(i).getCode() + "0");
 
-                    System.out.println(passedNodes.get(i).getChildLeft());
+                    System.out.println("LEFT [adding 0 to code]\t\t");
 
-                    passedNodes.get(i).getChildLeft().setCode(passedNodes.get(i).getCode());
 
-                    passedNodes.get(i).getChildLeft().setCode(passedNodes.get(i).getCode());
-
-                    System.out.print("LEFT [adding 0 to code]\t\t");
-                    System.out.println("new code for: " + passedNodes.get(i).getC() + " = " +
-                            passedNodes.get(i).getCode());
+                    setChildrenCodes(passedNodes, i);
                 }
 
 
                 if (passedNodes.get(i).getSide().equals("Right")) {
+                    System.out.println("RIGHT [adding 1 to code]\t\t");
                     passedNodes.get(i).setCode(passedNodes.get(i).getCode() + "1");
 
-                    System.out.println();
-                    if (passedNodes.get(i).getChildLeft() != null) ;
-                    passedNodes.get(i).getChildLeft().setCode(passedNodes.get(i).getCode());
-                    if (passedNodes.get(i).getChildRight() != null) ;
-                    passedNodes.get(i).getChildLeft().setCode(passedNodes.get(i).getCode());
 
-                    System.out.print("RIGHT [adding 1 to code]\t\t");
-                    System.out.println("new code for: " + passedNodes.get(i).getC() + " = " +
-                            passedNodes.get(i).getCode());
+                    setChildrenCodes(passedNodes, i);
+
                 }
-            }
-           /* if (passedNodes.get(i).getChildLeft() != null && i < passedNodes.size() - 1) {
-                passedNodes.get(i).getChildLeft().setCode(passedNodes.get(i).getCode() + "1");
-                System.out.print("CHILD [adding 1 to code]\t\t");
                 System.out.println("new code for: " + passedNodes.get(i).getC() + " = " +
                         passedNodes.get(i).getCode());
             }
-            if (passedNodes.get(i).getChildRight() != null) {
-                passedNodes.get(i).getChildRight().setCode(passedNodes.get(i).getChildRight().getCode() + "0");
-                System.out.print("CHILD [adding 0 to code]\t\t");
-                System.out.println("new code for: " + passedNodes.get(i).getC() + " = " +
-                        passedNodes.get(i).getCode());
-            }*/
+            System.out.println();
         }
 
 
         for (Node n : passedNodes) {
-            if (!String.valueOf(n.getC()).contains("Parent"))
+            if (!String.valueOf(n.getC()).contains("Parent")) {
                 System.out.println(n.getC() + " code: " + n.getCode());
-            n.setCode("");
+                sumAfter += Integer.parseInt(n.getCode(), 2) * n.getValue();
+            }
+
+        }
+        System.out.println("SUM: " + sumAfter);
+    }
+
+    private static void setChildrenCodes(ArrayList<Node> passedNodes, int i) {
+        if (passedNodes.get(i).getC().contains("Parent")) {
+
+            passedNodes.get(i).getChildLeft().setCode(passedNodes.get(i).getCode());
+            System.out.println("new child L code = " + passedNodes.get(i).getChildLeft().getCode());
+            passedNodes.get(i).getChildRight().setCode(passedNodes.get(i).getCode());
+            System.out.println("new child R code = " + passedNodes.get(i).getChildRight().getCode());
         }
     }
 
